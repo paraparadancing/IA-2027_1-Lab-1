910ab70eac9b678be522c4a514026751a820dd45
paraparadancing <145167458+paraparadancing@users.noreply.github.com>
2026-09-07 14:35:21 -0600

    feat: creación de las clases de los agente

---
+++ src/agents/basic/goal_based.py
@@ -0,0 +1,7 @@
+from src.agents.agent import Agent
+
+class GoalBasedAgent(Agent):
+        # TODO: Programar la lógica del agente basado en objetivos
+        # **ESTA ES LA FUNCIÓN PRINCIPAL DESDE DONDE PROGRAMAS EL AGENTE.** 
+        ...
+    
---
+++ src/agents/basic/simple_reflex.py
@@ -0,0 +1,7 @@
+from src.agents.agent import Agent
+
+class SimpleRelfexAgent(Agent):
+        # TODO: Programar la lógica del agente simple
+        # **ESTA ES LA FUNCIÓN PRINCIPAL DESDE PROGRAMAS EL AGENTE.** 
+        ...
+    
