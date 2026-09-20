a7694fb1dc825766837d952a164307cbc2b9889f
paraparadancing <145167458+paraparadancing@users.noreply.github.com>
2026-09-20 16:17:20 -0600

    Modificación del script bash de arranque

--- Battleship.sh
+++ Battleship.sh
@@ -1,1 +1,12 @@
-sh -c "python3 src/main.py"
+#!/bin/bash
+
+if [[ -e venv/bin/activate ]]; then
+	source venv/bin/activate
+else
+	echo "ERROR: No se encontró el entorno virtual de Python"
+	exit 1
+fi
+
+python3 -m pip install -r requirements.txt
+
+python3 src/main.py
