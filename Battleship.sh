497ef78565a2c5471b929326cf3aef0c44488878
paraparadancing <145167458+paraparadancing@users.noreply.github.com>
2026-09-20 18:13:12 -0600

    Revert "Modificación del script bash de arranque"
    This reverts commit a7694fb1dc825766837d952a164307cbc2b9889f.

--- Battleship.sh
+++ Battleship.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-if [[ -e venv/bin/activate ]]; then
-	source venv/bin/activate
-else
-	echo "ERROR: No se encontró el entorno virtual de Python"
-	exit 1
-fi
-
-python3 -m pip install -r requirements.txt
-
-python3 src/main.py
+sh -c "python3 src/main.py"
